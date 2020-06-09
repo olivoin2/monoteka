b15cdacb5b3b5a69132738446999385ac24cd3b1
p_motion <ss@pmotion.ru>
2020-06-09 14:09:47 +0300

    first

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+     <nuxt/>
+</template>
+
+<style lang="scss">
+  @import url(../assets/global.scss);
+  @import url(../assets/grid.scss);
+  body {
+  	font-family: 'Oswald', sans-serif;
+  }
+  .dis-flex {
+  	display: flex;
+  }
+  .text-center {
+	    text-align: center;
+	}
+</style>
+
